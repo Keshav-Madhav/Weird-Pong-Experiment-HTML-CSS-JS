e914044be98c1541874951a179d6eff1ee6d986d
Keshav Madhav <keshav2552003@gmail.com>
2024-02-01 09:45:07 +0530

    Innitial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerHeight;
+const height = canvas.height = window.innerHeight;
