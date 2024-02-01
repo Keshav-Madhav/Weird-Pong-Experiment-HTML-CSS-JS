6f0a71b58fc2f80557bef4af1b063deffc441d52
Keshav Madhav <keshav2552003@gmail.com>
2024-02-01 10:15:20 +0530

    Grid creation complete

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerHeight;
-const height = canvas.height = window.innerHeight;
+// Make sure the canvas is a square
+const size = Math.min(window.innerWidth, window.innerHeight);
+var width = canvas.width = size;
+var height = canvas.height = size;
+
+const rowCount = 20;
+const colCount = 20;
+const cellSize = Math.floor(size / rowCount);
+
+width = canvas.width = cellSize * colCount;
+height = canvas.height = cellSize * rowCount;
+
+const createGrid = () => {
+  var grid = [];
+  const halfColCount = Math.floor(colCount / 2); // Calculate the half here
+  for (var i = 0; i < rowCount; i++) {
+    grid[i] = [];
+    for (var j = 0; j < colCount; j++) {
+      if (j < halfColCount) {
+        grid[i][j] = 0; // Left half
+      } else {
+        grid[i][j] = 1; // Right half
+      }
+    }
+  }
+  return grid;
+}
+var grid = createGrid();
+
+
+// Function to draw a grid on a canvas
+function drawGrid(ctx, grid, cellSize) {
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] == 1) {
+        ctx.fillStyle = 'rgb(200, 225, 245)';
+        ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+      }
+      else if(grid[i][j] == 0){
+        ctx.fillStyle = 'rgb(30, 065, 085)';
+        ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+      }
+    }
+  }
+}
+
+setInterval(() => {
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillRect(0, 0, width, height);
+
+  drawGrid(ctx, grid, cellSize);
+}, 1000 / 60);
